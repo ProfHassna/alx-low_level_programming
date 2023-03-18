03cc4613dfc2568596cc918ee7bf0d0bde891c06
Hassna <hassna.aaraichi@gmail.com>
2023-03-18 06:38:18 -0700

    combin 2 digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A program that prints combinations of two digits
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i, n;
+
+	i = 0;
+	while (i <= 9)
+	{
+		for (n = 1; n <= 9; ++n)
+		{
+			if (i < n)
+			{
+				putchar(48 + i);
+				putchar(48 + n);
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
