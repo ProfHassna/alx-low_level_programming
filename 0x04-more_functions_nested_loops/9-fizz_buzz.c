b202db4f55eb9f8b811924e9de3014f2ee28ddb3
Hassna <hassna.aaraichi@gmail.com>
2023-03-24 14:46:14 -0700

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,19 +13,26 @@ int main(void)
 	i = 1;
 	while (i <= 100)
 	{
+	if ((i % 3 == 0) && (i % 5 == 0))
+		printf("FizzBuzz ");
+	else
+	{
 		if (i % 3 == 0)
 			printf("Fizz ");
 		else
-			if (i % 5 == 0)
-			{
-			       if (i != 100)
-					printf("Buzz ");
-		      	 	else
-				       printf("Buzz");
-			}    
+		{
+			if (i % 5 == 0 && i != 100)
+				printf("Buzz ");
 			else
-				printf("%d ", i);
-		i++;
+			{
+				if (i == 100)
+					printf("Buzz");
+				else
+					printf("%d ", i);
+			}
+		}
+	}
+	i++;
 	}
 	printf("\n");
 	return (0);
