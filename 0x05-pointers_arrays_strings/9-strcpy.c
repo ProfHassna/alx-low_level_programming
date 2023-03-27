0d7c1a5117e01bf5c161accfc159636f4ef6df99
Hassna <hassna.aaraichi@gmail.com>
2023-03-27 16:15:38 -0700

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -20,8 +20,8 @@ char *_strcpy(char *dest, char *src)
 	}
 	for (i = 0 ; i < n && src[i] != '\0' ; i++)
 		dest[i] = src[i];
-	for ( ; i < n ; i++)
-		dest[i] = '\0';
+
+	dest[i] = '\0';
 
 	return (dest);
 }
