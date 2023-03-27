9b2b1b6a5b57aa7e07e2766d2c8abf985f539d87
Hassna <hassna.aaraichi@gmail.com>
2023-03-27 11:32:31 -0700

    puts 2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * puts2 - a function that prints every other character of a string
+ * @str: param to print
+ * Return: nothings.
+ */
+
+void puts2(char *str)
+{
+	int i, j;
+
+	j = 0;
+	while (str[j] != '\0')
+		j++;
+	for (i = 0; i <= j; i++)
+	{
+		if (i % 2 == 0)
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
