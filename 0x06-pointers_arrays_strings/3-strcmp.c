57ba9fee7cbed18caeb913a6e44e0e6153742d3b
Hassna <hassna.aaraichi@gmail.com>
2023-03-31 04:01:02 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,15 +4,24 @@
 
 /**
  * _strcmp - a function that compares two strings.
- * @*s1: string to compare
- * @*s2: string to compare
+ * @s1: string to compare
+ * @s2: string to compare
  * Return: returns an integer indicating the result of the comparison
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	int result;
+	int i;
 
-	result = strcmp(s1, s2);;
-	return (result);
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+	return (0);
+
 }
