4d73101a2cbb19699ef7f1eb29c68ad3d35332c4
Hassna <hassna.aaraichi@gmail.com>
2023-03-31 03:10:05 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,9 +14,11 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int j;
 
-	for (j = 0; j < n && src[j] != '\0'; j++)
+	j = 0;
+	while ((j < n) && (src[j] != '\0'))
 	{
-			dest[j] = src[j];
+		dest[j] = src[j];
+		j++;
 	}
 	while (j < n)
 	{
