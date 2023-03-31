d71fdf5819d63069148408686d413d0473505f60
Hassna <hassna.aaraichi@gmail.com>
2023-03-31 02:59:03 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,16 +4,25 @@
 
 /**
  * *_strncpy - a function that copies a string
- * @*dest: string to append to
- * @*src: string to append
+ * @dest: string to append to
+ * @src: string to append
  * @n: num of bytes
  * Return: Pointer to the result.
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	char *ptr;
+	int j;
 
-	ptr = strncpy(dest, src, n);;
-	return (ptr);
+	for (j = 0; j < n && src[j] != '\0'; j++)
+	{
+			dest[j] = src[j];
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+
+	return (dest);
 }
