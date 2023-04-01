4b0062965849dc4d0f7b0daee7741eb6e061c1a2
Hassna <hassna.aaraichi@gmail.com>
2023-04-01 04:46:20 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rot13 - encode into rot13 speak
+ * @n: input value
+ * Return: n value
+ */
+char *rot13(char *n)
+{
+	int i, j;
+	char s1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char s2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (n[i] == s1[j])
+			{
+				n[i] = s2[j];
+				break;
+			}
+		}
+	}
+return (n);
+}
+
