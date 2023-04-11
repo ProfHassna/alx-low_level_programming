99c2de7c77dd9f4f0648461e63124e5a0ea2e575
Hassna <hassna.aaraichi@gmail.com>
2023-04-11 09:20:31 -0700

    concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,11 +16,12 @@ char *str_concat(char *s1, char *s2)
 	int x = 0;
 	int y = 0;
 	int size;
+	char *null = "";
 
 	if (s1 == NULL)
-		*s1 = "";
+		s1 = null;
 	if (s2 == NULL)
-		*s2 = "";
+		s2 = null;
 	while (s1[x] != '\0')
 		x++;
 	while (s2[y] != '\0')
