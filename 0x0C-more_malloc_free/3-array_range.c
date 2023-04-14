b8724ba477b6a36e7ec85a1cce15b32deb6d3276
Hassna <hassna.aaraichi@gmail.com>
2023-04-14 04:11:54 -0700

    arrange an array

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,20 +12,20 @@
 
 int *array_range(int min, int max)
 {
-	int *p, i, s;
+	int *p, i, s, j;
 
 	if (min > max)
 		return (0);
-
 	s = max - min + 1;
 	p = malloc(sizeof(int) * s + 1);
 	if (p == 0)
 		return (0);
 
-	for (i = 0; min <= max; i++)
+	j = min;
+	for (i = 0; j <= max; i++)
 	{
-		*(p + i) = min;
-		min ++;
+		*(p + i) = j;
+		j ++;
 	}
 	*(p + i) = '\0';
 	return (p);
