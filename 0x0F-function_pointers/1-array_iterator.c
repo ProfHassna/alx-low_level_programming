8bf59ded70a503e27acbcf8ea694fdcb1ddb79f1
Hassna <hassna.aaraichi@gmail.com>
2023-04-19 07:57:04 -0700

    choice array action

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,7 +2,7 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - executes a function given as a parameter on each element of an array.
+ * array_iterator - executes a func given as a param on each elmt of an array
  * @array: an array
  * @size: size of the array
  * @action: pointer to teh function you need
