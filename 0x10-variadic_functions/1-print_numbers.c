06085c47e18d5e97b692b6ad92a375aa76f3d80e
Hassna <hassna.aaraichi@gmail.com>
2023-04-20 08:52:08 -0700

    print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,6 +15,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list ap;
 	unsigned int i;
 
+	if (n == 0)
+	{
+		printf("\n");
+		return;
+	}
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 		if ((i == (n - 1)) || (separator == NULL))
