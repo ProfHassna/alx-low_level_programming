27c7f61acf5966e358a6c540a404a0ab539fa990
Hassna <hassna.aaraichi@gmail.com>
2023-05-01 12:01:06 -0700

    free 2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,11 +10,11 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp, *h = *head;
+	listint_t *temp;
 
 	while (*head)
 	{
-		temp = h->next;
+		temp = (*head)->next;
 		free(*head);
 		*head = temp;
 	}
