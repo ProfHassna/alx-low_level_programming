6fb670ff41c76092fd9b2eec4acf0a7593aa6da2
Hassna <hassna.aaraichi@gmail.com>
2023-05-02 03:00:55 -0700

    add a noe at index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,10 +18,16 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i;
 
 	if (*head == NULL)
-		return (0);
+		return (NULL);
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
-		return (0);
+		return (NULL);
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
 	new->n = n;
 	for (i = 0; i < (idx - 1); i++)
 	{
