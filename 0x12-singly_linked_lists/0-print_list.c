78f664bc6798982afc3ecc07cbc3f4889267e83e
Hassna <hassna.aaraichi@gmail.com>
2023-05-02 04:28:39 -0700

    update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,18 +12,14 @@ size_t print_list(const list_t *h)
 {
 	size_t i = 0;
 
-	if (h == NULL)
-		return (0);
-
-	while (h != NULL)
+	while (h)
 	{
-		if (h->str == NULL)
-		       printf("[%d] %s\n", 0, "(nil)");
+		if (!h->str)
+			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n", h->len, h->str);
-		i++;
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
+		i++;
 	}
-
 	return (i);
 }
