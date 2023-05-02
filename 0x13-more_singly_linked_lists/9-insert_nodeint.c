d0d4611d5a0f6c7bce6488a071794ef9e2590351
Hassna <hassna.aaraichi@gmail.com>
2023-05-02 02:52:11 -0700

    add a noe at index

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - add an elements at index
+ * @head: pointer to the first element
+ * @idx: position where to add
+ * @n: val of node
+ * Return: the address of the new element (on success)
+ * return NULL on fail
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new, *temp = *head;
+	unsigned int i;
+
+	if (*head == NULL)
+		return (0);
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (0);
+	new->n = n;
+	for (i = 0; i < (idx - 1); i++)
+	{
+		if (temp->next != NULL)
+			temp = temp->next;
+	}
+	new->next = temp->next;
+	temp->next = new;
+	return (new);
+}
