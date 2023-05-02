3196d8aacf174209ef9717e21f459be7d4c5be95
Hassna <hassna.aaraichi@gmail.com>
2023-05-02 04:33:23 -0700

    lenthgh of list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * list_len - returns the number of elements
+ * @h: singly linked list.
+ * Return: number of elements in the list.
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
